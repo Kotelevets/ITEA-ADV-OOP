a774ffbfe4fed20ad3b770e853f28e45655dbd30
Kotelevets <kotya.aa@gmail.com>
2018-11-30 04:00:21 +0200

    Add __constructor and static members (property and method)

--- src/Person.php
+++ src/Person.php
@@ -2,16 +2,31 @@
 
 class Person
 {
+    const MAX_POSSIBLE_AGE = 50;
+
+    private static $maxAge = 0;
+
     private $username;
     private $email;
     private $age;
 
     /**
-     * @param $username
+     * @return int
      */
-    public function setUsername($username)
+    public static function getOldest()
+    {
+        return self::$maxAge;
+    }
+
+    /**
+     * Person constructor.
+     * @param $username
+     * @param $email
+     */
+    public function __construct($username, $email)
     {
         $this->username = $username;
+        $this->email    = $email;
     }
 
     /**
@@ -23,18 +38,32 @@ class Person
     }
 
     /**
-     * @param $email
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-
-    /**
      * @return mixed
      */
     public function getEmail()
     {
         return $this->email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function setAge($age)
+    {
+        if ($age > self::MAX_POSSIBLE_AGE) {
+            echo "Can't create person with provided age " . $age . \PHP_EOL;
+            return;
+        }
+
+        $this->age = $age;
+
+        if ($age > self::$maxAge) {
+            self::$maxAge = $age;
+        }
     }
 }

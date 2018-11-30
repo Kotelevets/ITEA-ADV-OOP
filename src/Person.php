e5f27b9112d5d10218e856e900f88a4a68be51b7
Kotelevets <kotya.aa@gmail.com>
2018-11-30 02:46:51 +0200

    Add getter/setter and use it

--- src/Person.php
+++ src/Person.php
@@ -2,17 +2,39 @@
 
 class Person
 {
-    public $username;
-    public $email;
-    public $age;
+    private $username;
+    private $email;
+    private $age;
 
+    /**
+     * @param $username
+     */
     public function setUsername($username)
     {
         $this->username = $username;
     }
 
+    /**
+     * @return mixed
+     */
     public function getUsername()
     {
         return $this->username;
+    }
+
+    /**
+     * @param $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
     }
 }
